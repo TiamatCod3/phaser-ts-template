b4a6cc733e482daf67182f5af791556965e408d0
TiamatCod3 <ti_matos@id.uff.br>
2023-02-23 00:35:12 -0300

    finalizando configuracoes iniciais

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/",
+    base: '/phaser-ts-template/',
     server:{
         port:3000
     }
